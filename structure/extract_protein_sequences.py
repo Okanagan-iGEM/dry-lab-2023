fed8bc8974a7c980e7c960cd033b2d137d2a0452
Sharan S <sharansr@pm.me>
2023-05-15 12:43:38 -0700

    Fix slicing syntax

--- structure/extract_protein_sequences.py
+++ structure/extract_protein_sequences.py
@@ -13,15 +13,11 @@ VP2 = seq[211:464]
 VP1 = seq[484:901]
 VP3 = seq[901:1159]
 
-# VP2 = seq[211:464]
-# VP1 = seq[484:901]
-# VP3 = seq[900:1159]
+VP2_record = SeqRecord(Seq(VP2), id='VP2', description='')
+VP1_record = SeqRecord(Seq(VP1), id='VP1', description='')
+VP3_record = SeqRecord(Seq(VP3), id='VP3', description='')
 
-# VP2_record = SeqRecord(Seq(VP2), id='VP2', description='')
-# VP1_record = SeqRecord(Seq(VP1), id='VP1', description='')
-# VP3_record = SeqRecord(Seq(VP3), id='VP3', description='')
+records = [VP2_record, VP1_record, VP3_record]
 
-# records = [VP2_record, VP1_record, VP3_record]
-
-# output_file = open('output.faa', 'w')
-# SeqIO.write(records, output_file, 'fasta')
+output_file = open('output.faa', 'w')
+SeqIO.write(records, output_file, 'fasta')
